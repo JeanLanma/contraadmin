91ddf0c01ab5b88869e14c95113a706f51647667
Jean <jeanmacario048@gmail.com>
2023-12-27 11:39:59 -0600

    Build\ 27.12.2023-1112v2

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -14,7 +14,7 @@ defineProps({
 <template>
     <Head title="Contraadmin" />
 
-    <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen  bg-center bg-gray-100  dark:bg-gray-900 selection:bg-red-500 selection:text-white bg-[url('assets/landingcontraadmin-gradient.png')]" style="background-size: cover;">
+    <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen  bg-center bg-gray-100  dark:bg-gray-900 selection:bg-red-500 selection:text-white bg-[url('/assets/landingcontraadmin-gradient.png')]" style="background-size: cover;">
     
         <div v-if="canLogin" class="sm:fixed sm:top-0 sm:end-0 p-6 text-end z-10 w-full">
 
